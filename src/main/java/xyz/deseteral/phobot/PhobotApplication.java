0bcf82ac0b76db0ac6dfccebc2a904d1fd2fca54
Deseteral <deseteral@gmail.com>
2018-03-28 19:43:18 +0200

    Spring Boot starter

---
+++ src/main/java/xyz/deseteral/phobot/PhobotApplication.java
@@ -0,0 +1,11 @@
+package xyz.deseteral.phobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhobotApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PhobotApplication.class, args);
+    }
+}
