6932099e387c26772365454c91a34f14b4c42f49
Deseteral <deseteral@gmail.com>
2018-04-03 11:00:35 +0200

    Foo component

--- src/main/java/xyz/deseteral/phobot/modules/FooModule.java
+++ src/main/java/xyz/deseteral/phobot/modules/FooModule.java
@@ -1,9 +1,11 @@
 package xyz.deseteral.phobot.modules;
 
+import org.springframework.stereotype.Component;
 import xyz.deseteral.phobot.invocation.InvocationProcessor;
 import xyz.deseteral.phobot.invocation.model.Invocation;
 import xyz.deseteral.phobot.invocation.model.Response;
 
+@Component
 public class FooModule implements InvocationProcessor {
     @Override
     public Response process(Invocation invocation) {
